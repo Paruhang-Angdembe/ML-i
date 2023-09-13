d55837cc82f0c6bb41158853274de6a361602cf8
Paruhang Angdembe <52775221+ParuhangAngdembe@users.noreply.github.com>
2023-09-13 10:56:56 -0400

    added comments on .py

--- task_one/webScrapping.py
+++ task_one/webScrapping.py
@@ -1,3 +1,5 @@
+# Note: Changes for pull request
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -5,14 +7,14 @@ url = "https://medium.com/towards-data-science/a-practical-guide-to-linear-regre
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
-paragraph = doc.find('p', id='f23b')
+paragraph = doc.find("p", id="f23b")
 outText = paragraph.text
 
 print(outText)
 
 # Saving Extracted Content to Text File
 out = "out.txt"
-with open(out, 'w') as file:
+with open(out, "w") as file:
     file.write(str(outText))
     file.close()
 
